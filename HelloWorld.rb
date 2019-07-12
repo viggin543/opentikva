8a1f93b2eb59df99532ce4d0425dbc06e4e40881
Domrev Igor <domrevigor@gmail.com>
2019-07-12 16:52:25 +0300

    a hello world formla

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -2,6 +2,7 @@ class Helloworld < Formula
   desc "an elf executable (golang) that prints hello world"
   homepage "https://github.com/viggin543/opentikva"
   version "0.3"
+  sha256 "310c2292b237daffe673f0ec06d3a0202104b03017d00dad9085614ba61cbecc"
 
   url "https://opentikva.s3.amazonaws.com/hello", :using => :curl
 
