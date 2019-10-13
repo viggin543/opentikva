6c7976a27faf73392fa8715451745ae92d04a7b8
Domrev Igor <domrevigor@gmail.com>
2019-10-13 22:26:43 +0300

    .

--- PullRequests.rb
+++ PullRequests.rb
@@ -1,21 +1,20 @@
-class Lpr < Formula
+class PullRequests < Formula
   desc "an elf executable (golang) that prints hello world"
   homepage "https://github.com/viggin543/go_alfred/tree/master/github-pull-requests"
-  sha256 "6e238fd47be9fb52c12672e68266caa8cf9ceee8467b621259de7e12415117c6"
 
   version "0.3"
   
 
-  url "https://opentikva.s3.amazonaws.com/lpr", :using => :curl
+  url "https://opentikva.s3.amazonaws.com/PullRequests", :using => :curl
 
 
   def install
     system "echo" , "fetching binaries..."
-    bin.install "lpr"
+    bin.install "PullRequests"
   end
 
   def uninstall
-    bin.uninstall "lpr"
+    bin.uninstall "PullRequests"
   end
 
   def post_install
