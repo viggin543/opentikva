066f3aa3cc9905ff85637b17a4dd5be1af8f7267
Domrev Igor <domrevigor@gmail.com>
2019-10-13 22:22:31 +0300

    .

--- Lpr.rb
+++ Lpr.rb
@@ -1,4 +1,4 @@
-class PullRequests < Formula
+class Lpr < Formula
   desc "an elf executable (golang) that prints hello world"
   homepage "https://github.com/viggin543/go_alfred/tree/master/github-pull-requests"
   sha256 "6e238fd47be9fb52c12672e68266caa8cf9ceee8467b621259de7e12415117c6"
