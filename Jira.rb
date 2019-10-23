7a4a57b302bacc0df23dddfc48624660b341afa9
Domrev Igor <domrevigor@gmail.com>
2019-10-23 13:20:30 +0300

    .

--- Jira.rb
+++ Jira.rb
@@ -1,7 +1,7 @@
 class Jira < Formula
   desc "jira command line tool. $ jira -h for usage info"
   homepage "https://github.com/viggin543/opentikva/tree/master/scripts/atlassian"
-  sha256 "074e76ee998f5003e40ae0a2e474ef092561331a0ec051c33961a40a42b38514"
+  # sha256 "074e76ee998f5003e40ae0a2e474ef092561331a0ec051c33961a40a42b38514"
   version "2"
 
   url "https://raw.githubusercontent.com/viggin543/opentikva/master/scripts/atlassian/jira", :using => :curl
