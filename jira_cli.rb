8e65302c5062f173008137c37b15f0d146bb0cb5
Domrev Igor <domrevigor@gmail.com>
2019-11-20 21:46:20 +0200

    adding jira cli formula

--- jira_cli.rb
+++ jira_cli.rb
@@ -1,4 +1,4 @@
-class jira_cli < Formula
+class JiraCli < Formula
   desc "jira command line tool. $ jira_cli -h for usage info"
   homepage "https://github.com/viggin543/jira_cli"
   # sha256 "667e0e03aafc984a27e869ed544d9461fbf9ea0f32d8b16611dbdaf74a18a009"
