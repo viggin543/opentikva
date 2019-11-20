44f6e0725c2aab07fdc930e9b7d42173a49144d2
Domrev Igor <domrevigor@gmail.com>
2019-11-20 21:40:22 +0200

    adding jira cli formula

--- Jira.rb
+++ Jira.rb
@@ -1,7 +1,7 @@
 class Jira < Formula
   desc "jira command line tool. $ jira -h for usage info"
   homepage "https://github.com/viggin543/opentikva/tree/master/scripts/atlassian"
-  # sha256 "074e76ee998f5003e40ae0a2e474ef092561331a0ec051c33961a40a42b38514"
+  sha256 "667e0e03aafc984a27e869ed544d9461fbf9ea0f32d8b16611dbdaf74a18a009"
   version "3"
 
   url "https://raw.githubusercontent.com/viggin543/opentikva/master/scripts/atlassian/jira", :using => :curl
