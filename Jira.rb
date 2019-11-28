4a0c810bcebd4b3f7100f7214e40c6ea15a072e7
Domrev Igor <domrevigor@gmail.com>
2019-11-28 09:14:16 +0200

    adding checksum of jira binary

--- Jira.rb
+++ Jira.rb
@@ -1,6 +1,7 @@
 class Jira < Formula
   desc "jira command line tool. $ jira -h for usage info"
   homepage "https://github.com/viggin543/jira"
+  sha256 "c779ff5c7e73e3df4981312b6fc3c5769df400bbbe663ce3345f97b60d1fa147"
   version "1"
 
   url "https://opentikva.s3.amazonaws.com/jira", :using => :curl
